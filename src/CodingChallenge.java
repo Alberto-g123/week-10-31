5abab7a69dfa924400336cf6983d40f7f5e0cdc5
Alberto-g123 <albertogutierrez700@gmail.com>
2022-10-31 14:01:46 -0400

    coding challenge

--- src/CodingChallenge.java
+++ src/CodingChallenge.java
@@ -4,6 +4,13 @@ import java.util.Scanner;
 public class CodingChallenge {
 
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String string = sc.nextLine();
+        System.out.println(atbashCipher(string));
+    }
+
     public static String atbashCipher(String string){
         String cipher = "";
         String alphabet = "abcdefghijklmnopqrstuvwxyz";
@@ -15,28 +22,21 @@ public class CodingChallenge {
             }else if(Character.isLetter(letter) == false){
                 cipher += String.valueOf(letter);
             }else{
-            for(int x = 0; x < alphabet.length(); x++){
-                if(letter == blank){
-                    cipher += " ";
-                }else if(Character.isUpperCase(letter)){
-                    if(letter == alphabet.toUpperCase().charAt(x)){
-                        cipher += alphabet.toUpperCase().charAt(25 - x);
+                for(int x = 0; x < alphabet.length(); x++){
+                    if(letter == blank){
+                        cipher += " ";
+                    }else if(Character.isUpperCase(letter)){
+                        if(letter == alphabet.toUpperCase().charAt(x)){
+                            cipher += alphabet.toUpperCase().charAt(25 - x);
+                        }
+                    }else if(Character.isLowerCase(letter)){
+                        if(letter == alphabet.charAt(x)){
+                            cipher += alphabet.charAt(25 - x);
+                        }
                     }
-                }else if(Character.isLowerCase(letter)){
-                    if(letter == alphabet.charAt(x)){
-                        cipher += alphabet.charAt(25 - x);
-                    }
-                }
 
-            }}
+                }}
         }
         return cipher;
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        String string = sc.nextLine();
-        System.out.println(atbashCipher(string));
     }
 }
